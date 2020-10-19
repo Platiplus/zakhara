942d914ea85107ad04ad81f7fb7cc6be5cda06d3
Platiplus <quazarweb@gmail.com>
2020-10-19 19:45:47 -0300

    Implemented Item base class

--- backend/Zakhara.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Zakhara.Domain
-{
-	public class Class1
-	{
-	}
-}
---
+++ backend/Zakhara.Domain/Items/Base/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zakhara.Domain.Items.Base
+{
+	class Item
+	{
+		public Guid Id { get; set; }
+		public bool Consumable { get; set; }
+		public String Description { get; set; }
+		public String Name { get; set; }
+		public float Price { get; set; }
+		public float Weight { get; set; }
+	}
+}
